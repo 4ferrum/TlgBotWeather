2caa00580c912dce7555d5d3c7316717029899af
4ferrum <59420704+4ferrum@users.noreply.github.com>
2020-01-04 22:53:52 +0500

    py
    Исправлено расширение py

--- BotScript.py
+++ BotScript.py
@@ -4,14 +4,14 @@ import telebot
 owm = pyowm.OWM("04288af9666a79abd3a565b5028a54bd", language='ru')
 bot = telebot.TeleBot("840679200:AAGvoQ0LYO7BsJE4iB7XmAOmEfSd8qFteQs")
 
-status = w.get_detailed_status()
 
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
     observation = owm.weather_at_place(message.text)
     w = observation.get_weather()
-    temt = w.get_temperature('celsius')['temp']
+    temp = w.get_temperature('celsius')['temp']
+    status = w.get_detailed_status()
     answer = 'В городе ' + message.text + ' сейчас ' + status + "\n"
     answer += 'Температура сейчас в районе ' + str(temp) + '\n\n'
     bot.send_message(message.chat.id, answer)
